9710b679370cd85c84207651fc2d73b86ddb5485
N1kxo <nico9ms@hotmail.com>
2024-05-05 19:23:30 -0500

    Upload Files onto Repo

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // Definir el esquema del usuario
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { type: email, required: true, unique: true },
+  password: { type: password, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
